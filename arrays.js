86184495d39fab43ac8411ec9bfebf95320321d3
Hashmatullah Nazari <hn0153@gmail.com>
2020-07-13 13:13:15 -0500

    Create arrays.js

---
+++ arrays.js
@@ -0,0 +1,19 @@
+//Accessign variables using arrays indexes
+
+
+const productInfo = ['potato', 30, 3, true];
+
+console.log(productInfo[0]);
+console.log(productInfo[3])
+
+//Acessing variables usin arrays::
+console.log(`productName- ${productInfo[0]} productPrice- ${productInfo[1]} productQuantity- ${productInfo[2]} productIsAvailabel- ${productInfo[3]}`);
+
+
+
+
+console.log(`
+productName- ${productInfo[0]} 
+productPrice- ${productInfo[1]} 
+productQuantity- $ {productInfo[2]} 
+productIsAvailabel- ${productInfo[3]}`);
