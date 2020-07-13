e58c78af51920f837b0b5fd39c998d7eb5007a79
Hashmatullah Nazari <hn0153@gmail.com>
2020-07-13 13:08:25 -0500

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,31 @@
+const productName = "potato";
+
+const productPrice = 30;
+
+const productQuantity = 3;
+
+const productIsAvailabel = true;
+
+console.log("concatenation")
+                          console.log(productName + " " + productPrice + " " + productQuantity + " " + productIsAvailabel)
+console.log("concatenation")
+
+console.log("<------------------->")
+console.log(
+'productname:'+ productName + ' productprice: ' + productPrice + ' productquantity: ' + productQuantity +
+' productisavaiable: ' + productIsAvailabel
+  
+  )
+
+  console.log("<------------------->")
+
+
+
+console.log("template literal")
+console.log(`
+productName- ${productName} 
+productPrice- ${productPrice} 
+productQuantity- $ {productQuantity} 
+productIsAvailabel- ${productIsAvailabel}
+`);
+console.log("template literal")
